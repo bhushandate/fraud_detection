1dff3560bf0af19d456631d597c84284f776009a
bhushandate <bhushandate143@gmail.com>
2022-03-26 16:15:54 +0530

    corrected the code for sum function & added new mul function

---
+++ data_processing.py
@@ -0,0 +1,14 @@
+ print ('New code')
+ 
+ a = 5
+ b = 7
+
+ def sum(a,b):
+     c=a+b
+     return c
+ print("sum function")
+ 
+ def multiplication():
+     d=a*b
+     return d
+ print("multiplication function") 
